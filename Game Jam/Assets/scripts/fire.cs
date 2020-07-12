b8084cc70204001fcc841158a894d651a77e273c
ughleeperson <bboybasto@gmail.com>
2020-07-12 01:55:37 -0700

    RICO SHAY
    RICO SHAAAAAAAAAAAAAYYYY

--- Game Jam/Assets/scripts/fire.cs
+++ Game Jam/Assets/scripts/fire.cs
@@ -14,19 +14,20 @@ public class fire : MonoBehaviour
     private Vector2 lookDirection;
     private float lookAngle;
 
-
+    public float ms = 10f;
 
     // Update is called once per frame
     void Update()
     {
+
         lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
 
+
         if (Input.GetMouseButtonDown(0))
         {
             FireBullet();
-
         }
 
 
@@ -35,7 +36,7 @@ public class fire : MonoBehaviour
     private void FireBullet()
     {
         GameObject firedBullet = Instantiate(bullet, barrelTip.position, barrelTip.rotation);
-        firedBullet.GetComponent<Rigidbody2D>().velocity = barrelTip.up * 10f;
+        firedBullet.GetComponent<Rigidbody2D>().velocity = barrelTip.up * ms;
 
     }
 }
